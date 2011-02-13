388661402ebb5f2377c91958d798ec3c43fe534a
Haacked <devnull@localhost>
2011-02-13 12:59:09 -0800

    Added medium trust implementation for registering editable routes.

--- src/RouteMagic/RouteRegistrationExtensions.cs
+++ src/RouteMagic/RouteRegistrationExtensions.cs
@@ -1,13 +1,36 @@
-﻿using System.Web.Compilation;
+﻿using System;
+using System.Web.Compilation;
 using System.Web.Routing;
 using RouteMagic.Internals;
 
 namespace RouteMagic {
     public static class RouteRegistrationExtensions {
+        // This method requires full trust.
+        // See http://haacked.com/archive/2010/01/17/editable-routes.aspx for more details.
         public static void RegisterRoutes(this RouteCollection routes,
             string virtualPath) {
+            if (String.IsNullOrEmpty(virtualPath)) {
+                throw new ArgumentNullException("virtualPath");
+            }
+
             ConfigFileChangeNotifier.Listen(virtualPath,
               vp => routes.ReloadRoutes(vp));
+        }
+
+        // MEDIUM TRUST: Use this method. Place Routes.cs in the ~/App_Code directory.
+        // See http://haacked.com/archive/2010/01/18/editable-routes-in-medium-trust.aspx for more info.
+        public static void RegisterAppCodeRoutes(this RouteCollection routes) {
+            var type = BuildManager.GetType("Routes", false/*throwOnError*/);
+            if (type == null) {
+                throw new InvalidOperationException("Could not find a type Routes in the App_Code directory");
+            }
+
+            var registrar = Activator.CreateInstance(type) as IRouteRegistrar;
+            if (registrar == null) {
+                throw new InvalidOperationException("Could not find an instance of IRouteRegistrar");
+            }
+
+            registrar.RegisterRoutes(RouteTable.Routes);
         }
 
         static void ReloadRoutes(this RouteCollection routes, string virtualPath) {
