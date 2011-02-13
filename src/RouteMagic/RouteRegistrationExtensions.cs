65a1ec71af84d9dcf71c4b6243db317811d1216d
Haacked <devnull@localhost>
2011-02-13 13:11:25 -0800

    Rolling back that last change. The version of editable routes I originally committed was correct after all and works in Medium Trust.

--- src/RouteMagic/RouteRegistrationExtensions.cs
+++ src/RouteMagic/RouteRegistrationExtensions.cs
@@ -5,33 +5,13 @@ using RouteMagic.Internals;
 
 namespace RouteMagic {
     public static class RouteRegistrationExtensions {
-        // This method requires full trust. It doesn't require an app domain restart.
-        // See http://haacked.com/archive/2010/01/17/editable-routes.aspx for more details.
         public static void RegisterRoutes(this RouteCollection routes,
             string virtualPath) {
             if (String.IsNullOrEmpty(virtualPath)) {
                 throw new ArgumentNullException("virtualPath");
             }
 
-            ConfigFileChangeNotifier.Listen(virtualPath,
-              vp => routes.ReloadRoutes(vp));
-        }
-
-        // This method works in medium trust, but requires an app domain restart.
-        // Place Routes.cs in the ~/App_Code directory.
-        // See http://haacked.com/archive/2010/01/18/editable-routes-in-medium-trust.aspx for more info.
-        public static void RegisterAppCodeRoutes(this RouteCollection routes) {
-            var type = BuildManager.GetType("Routes", false/*throwOnError*/);
-            if (type == null) {
-                throw new InvalidOperationException("Could not find a type Routes in the App_Code directory");
-            }
-
-            var registrar = Activator.CreateInstance(type) as IRouteRegistrar;
-            if (registrar == null) {
-                throw new InvalidOperationException("Could not find an instance of IRouteRegistrar");
-            }
-
-            registrar.RegisterRoutes(RouteTable.Routes);
+            ConfigFileChangeNotifier.Listen(virtualPath, vp => routes.ReloadRoutes(vp));
         }
 
         static void ReloadRoutes(this RouteCollection routes, string virtualPath) {
