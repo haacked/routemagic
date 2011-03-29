31cf6f18aee44218d53a6fb5dbb0240150f2f40d
haacked <devnull@localhost>
2011-03-29 16:03:54 -0700

    Converting packages to create packages in the "out" directory.

---
+++ out/readme..txt
@@ -0,0 +1,2 @@
+Compiling the solution will place packages here. We don't want to check-in 
+the output packages, but we do want this folder to be created by default. :)
--- src/RouteMagic.Mvc/Properties/AssemblyInfo.cs
+++ src/RouteMagic.Mvc/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("RouteMagic.Mvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("RouteMagic.Mvc is a library of useful Routing helpers specific to ASP.NET MVC. See http://haacked.com/archive/2011/01/30/introducing-routemagic.aspx for more information.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3662ad8b-8cb5-4103-a85a-1d485b8658bc")]
