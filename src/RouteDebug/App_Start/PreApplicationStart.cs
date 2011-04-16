3ebdfbbe7e8db3290b1b3f3984a583b5e41a6b7d
Haacked <devnull@localhost>
2011-04-16 08:38:15 -0700

    Added a setting to enable/disable RouteMagic.

--- src/RouteDebug/App_Start/PreApplicationStart.cs
+++ src/RouteDebug/App_Start/PreApplicationStart.cs
@@ -1,12 +1,17 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using RouteDebug;
+using System;
 
 [assembly: PreApplicationStartMethod(typeof(PreApplicationStart), "Start")]
 
 namespace RouteDebug {
     public class PreApplicationStart {
         public static void Start() {
-            Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(RouteDebuggerHttpModule));
+            bool enabled = Convert.ToBoolean(ConfigurationManager.AppSettings["RouteDebugger:Enabled"]);
+            if (enabled) {
+                Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(RouteDebuggerHttpModule));
+            }
         }
     }
 }
