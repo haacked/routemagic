6a93bdeb02e6ced4fabf8d963cb643a29d73c211
Maarten Balliauw <maarten@maartenballiauw.be>
2012-07-18 16:08:53 +0200

    Added support for domain routing in MVC and WebAPI
    Added support for domain routing in MVC and WebAPI

---
+++ src/RouteMagic.WebApi/MvcRouteExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Routing;
+using RouteMagic.WebApi;
+
+namespace RouteMagic
+{
+    public static class HttpRouteExtenstions
+    {
+        public static Route MapHttp(this RouteCollection routes, string name, string domain, string url)
+        {
+            return routes.MapHttp(name, domain, url, null);
+        }
+
+        public static Route MapHttp(this RouteCollection routes, string name, string domain, string url, object defaults)
+        {
+            var route = new HttpDomainRoute(domain, url, defaults);
+            return route;
+        }
+    }
+}
---
+++ src/RouteMagic.WebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RouteMagic.Mvc")]
+[assembly: AssemblyDescription("RouteMagic.Mvc is a library of useful Routing helpers specific to ASP.NET MVC. See http://haacked.com/archive/2011/01/30/introducing-routemagic.aspx for more information.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3662ad8b-8cb5-4103-a85a-1d485b8658bc")]
