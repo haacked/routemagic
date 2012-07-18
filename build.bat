efb9a79da1f0b5ffdce00ae2c91aa5cefd1ded95
Maarten Balliauw <maarten@maartenballiauw.be>
2012-07-18 16:10:42 +0200

    Updated build script to use a single nuget.exe version under src
    Updated build script to use a single nuget.exe version under src

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 
-.\tools\nuget.exe pack src\RouteDebug\RouteDebugger.csproj -Build -Symbols -Properties Configuration=Release -o .\out
-.\tools\nuget.exe pack src\RouteMagic\RouteMagic.csproj -Build -Symbols -Properties Configuration=Release -o .\out
-.\tools\nuget.exe pack src\RouteMagic.Mvc\RouteMagic.Mvc.csproj -Build -Symbols -Properties Configuration=Release -o .\out
-.\tools\nuget.exe pack src\RouteMagic.WebApi\RouteMagic.WebApi.csproj -Build -Symbols -Properties Configuration=Release -o .\out
+.\src\.nuget\nuget.exe pack src\RouteDebug\RouteDebugger.csproj -Build -Symbols -Properties Configuration=Release -o .\out
+.\src\.nuget\nuget.exe pack src\RouteMagic\RouteMagic.csproj -Build -Symbols -Properties Configuration=Release -o .\out
+.\src\.nuget\nuget.exe pack src\RouteMagic.Mvc\RouteMagic.Mvc.csproj -Build -Symbols -Properties Configuration=Release -o .\out
+.\src\.nuget\nuget.exe pack src\RouteMagic.WebApi\RouteMagic.WebApi.csproj -Build -Symbols -Properties Configuration=Release -o .\out
