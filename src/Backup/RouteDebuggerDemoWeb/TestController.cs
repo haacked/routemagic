c67742b7a453c81a6e1c96df46cb7b27dbb704ee
Haacked <haacked@gmail.com>
2015-08-25 17:43:50 -0700

    Install Microsoft.AspNet.Mvc package
    To fix the build

---
+++ src/Backup/RouteDebuggerDemoWeb/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace RouteDebug
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SomeJsonService()
+        {
+            return Json(new { Title = "Some title" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/Backup/RouteMagicDemo.Web/HttpHandlers/HelloWorldHttpHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace RouteMagicDemo.Web.HttpHandlers {
+    public class HelloWorldHttpHandler : IHttpHandler {
+        public bool IsReusable {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ProcessRequest(HttpContext context) {
+            context.Response.Write("Hello World!");
+        }
+    }
+}
