b3cfbed491b5440cef88db036154ad135548421f
Phil Haack <haacked@gmail.com>
2016-04-13 14:36:44 -0700

    Update routedebugger to 2.1.5

--- src/RouteDebug/Properties/AssemblyInfo.cs
+++ src/RouteDebug/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("88784f4d-3799-4a7a-8e1d-16bbfbd77416")]
 
-[assembly: AssemblyVersion("2.1.4.0")]
-[assembly: AssemblyFileVersion("2.1.4.0")]
+[assembly: AssemblyVersion("2.1.5.0")]
+[assembly: AssemblyFileVersion("2.1.5.0")]
